2fc20a94af1ac0cf308bb31fb93f59a1c2006996
Karthik <dkkarthik2000@gmail.com>
2021-12-22 16:35:29 +0530

    completed

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,27 +1,52 @@
-import React from "react"
-import { matchPath, useLocation } from "react-router-dom"
-import AddTrip from "./addtrips"
-import Aside from "./aside"
-import Homepage from "./section"
 
+import trips from '../trips.json';
+import '../index.css'
+import React, { Component } from 'react'
+import sun from './sun.svg';
+import trek from './trek.svg';
+import trophy from './trophy.svg';
 
-
-
-const Home = () => {
-
-
-        const location = useLocation().pathname
-        console.log(location);
-    return (
-        <React.Fragment>
-            <Aside />
-           <section>
-        {
-            location === '/addtrip' ? <AddTrip/> : <Homepage/>
+ class Home extends Component {
+    trekCount=()=>{
+        var count = 0;
+        for(var i=0;i<trips.length;i++){
+            if(trips[i].type === 'Trek'){
+                count++;
+            }
         }
-           </section>
-        </React.Fragment>
-    )
+        return count;
+    }
+    clubCount=()=>{
+        var count = 0;
+        for(var i=0;i<trips.length;i++){
+            if(trips[i].type === 'Club'){
+                count++;
+            }
+        }
+        return count;
+    }
+    trophieCount=()=>{
+        var count = 0;
+        for(var i=0;i<trips.length;i++){
+            if(trips[i].type === 'Trophie'){
+                count++;
+            }
+        }
+        return count;
+    }
+    render() { 
+        return ( 
+            <>
+            <div className='home'>
+            <center><div className='trips'><span className='display'>{trips.length}</span>trips</div></center>
+            <div className='homelist'>
+            <div><span  className='display'>{this.trekCount()}</span><img src={trek} alt="" className='logo-small'/></div>
+            <div><span className='display'>{this.clubCount()}</span><img src={sun} alt="" className='logo-small'/></div>
+            <div><span className='display'>{this.trophieCount()}</span><img src={trophy} alt="" className='logo-small'/></div>
+            </div>
+            </div>
+            </>
+         );
+    }
 }
-
-export default Home
+export default Home;
